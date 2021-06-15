456c1ebda4abbb6efcb64807303c9748865f478e
chris6661 <74381468+chris6661@users.noreply.github.com>
2021-06-15 12:57:40 -0500

    added beginning navbar, time for work...

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,13 +1,32 @@
 import React from 'react'
-
+import { Nav } from 'react-bootstrap'; 
 const Navbar = () => {
     return (
-        <div className ="nav">
-           <navbar>
-               this is test text for nav, tis is test 2
-           </navbar>
+        <div>
+            <Nav>
+                <NavLink to = "/">
+                    <h1>logo</h1>
+                </NavLink>
+                <Bars />
+                <NavMenu>
+                    <NavLink to = "/about">
+                        About
+                    </NavLink>
+
+                    <NavLink to "/services">
+                        Services
+                    </NavLink>
+
+                    <NavLink to "/testimonials">
+                        Testimonials
+                    </NavLink>
+
+                </NavMenu>
+
+
+            </Nav>
         </div>
     )
 }
 
-export default Navbar;
+export default Navbar
