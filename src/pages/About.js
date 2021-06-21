7ba070fbeaaee4035f271e9c7e3c7b0ee6163e1a
chris6661 <74381468+chris6661@users.noreply.github.com>
2021-06-21 12:54:29 -0500

    added skeleton for about section

--- src/pages/About.js
+++ src/pages/About.js
@@ -2,8 +2,28 @@ import React from 'react'
 
 const About = () => {
     return (
-        <div>
-           test text for bout
+        <div className="about">
+           <h1>Your computer repair experts</h1>
+
+           <p>If you are in need of computer repair, sales, or IT services, our experienced technicians are here to help! with 2 convenient locations and services ranging from virus removal, troubleshooting, and network installations to screen and hardware replacements, computer sales, and general consultations, our experts at Aquilis are all you need for superioir tech services.</p>
+        <div/>
+
+        <div className="mission">
+            <h1>Our Mission & Purpose</h1>
+            <p>Our mission at Aquilis Computer Repair, Sales, and IT Services is to service the computer industry as a whole, starting with outstanding customer service and workmanship. Our speicalty is building custom computers for clients needing technology for personal, family, or business use, while also partaking in computer sales and services. </p>
+            <br/>
+            <p>In addition, we also have expert multimedia designs avaiolable from basic slideshow and video designs to advanced graphic design jobs. We offer a variety of computer parts and repair services in-store, in-home, and remotely for our customers convenience. Aquilis is your fast, friendly, reliable, and expert computer repair company for all of your technical needs!</p>
+
+            <h1>Our company and founder</h1>
+            <p>Aquilis was founded by Nicholas Aquilante III in 2007, the first shop located in Glassport, Pennsylvania. What started as a home based business, simply completing the computer repairs in customers homes, expanding to operating two different shops, meeting the business needs of individuals and corporations both, while always keeping the option to work from a customer's home.</p>
+            <br/>
+            <p>Nicholas always had an affinity for working on and fixing computers, taking his first computer apart at the age of 12 and fixing his first one, his mother's at the age of 13, and knew he found his niche.</p>
+
+            <h1>License Information</h1>
+
+        </div>
+
+
         </div>
     )
 }
