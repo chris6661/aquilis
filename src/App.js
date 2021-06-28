1b6277b26d08454d416d3d3ab85bda607f91920b
chris6661 <74381468+chris6661@users.noreply.github.com>
2021-06-28 12:15:34 -0500

    added react router, editing about page

--- src/App.js
+++ src/App.js
@@ -11,27 +11,44 @@ import Navbar from './components/Navbar';
 import Sell from './pages/Sell';
 import Services from './pages/Services'; 
 import Testimonials from './pages/Testimonials';  
+import { BrowserRouter as Router, Route } from 'react-router-dom'; 
+
 
 function App() {
   return (
-    <div className="App">
+    <>
+
+    <Router>
+
+    <div>
       
     <Header />
 
     <Navbar />
 
-    <Home />
-    <About />
-    <Services />
-    <Testimonials />
-    <Sell />
-    <Business />
-    <Locations />
-    <Book />
+    <div className="app-container">
+
+    <Route exact path = '/' component = {Home} />
+    <Route exact path = "/about" component = {About} />
+
+    <Route exact path = "/services" component = {Services} />
+    <Route exact path = "/testimonials" component = {Testimonials}/>
+
+    <Route exact path = "/sell" component = {Sell} />
+    <Route exact path = "/business" component = {Business} />
+
+    <Route exact path = "/locations" component = {Locations} />
+    <Route exact path = "/book" component = {Book}/>
+
+    </div>
 
     <Footer />
 
     </div>
+
+    </Router>
+
+    </>
   );
 }
 
--- src/pages/About.js
+++ src/pages/About.js
@@ -5,21 +5,24 @@ const About = () => {
         <div className="about">
            <h1>Your computer repair experts</h1>
 
-           <p>If you are in need of computer repair, sales, or IT services, our experienced technicians are here to help! with 2 convenient locations and services ranging from virus removal, troubleshooting, and network installations to screen and hardware replacements, computer sales, and general consultations, our experts at Aquilis are all you need for superioir tech services.</p>
+           <p>If you are in need of computer repair, sales, or IT services, our experienced technicians are here to help! with 2 convenient locations and services ranging from virus removal, troubleshooting, and network installations to screen and hardware replacements, computer sales, and general consultations, our experts at Aquilis are all you need for superior tech services.</p>
        
         <details className="mission">
-                <summary><h1>Our Mission & Purpose</h1></summary>
+                <summary className="purpose">Our Mission & Purpose</summary>
                 <p>Our mission at Aquilis Computer Repair, Sales, and IT Services is to service the computer industry as a whole, starting with outstanding customer service and workmanship. Our speicalty is building custom computers for clients needing technology for personal, family, or business use, while also partaking in computer sales and services. </p>
                 <br/>
                 <p>In addition, we also have expert multimedia designs avaiolable from basic slideshow and video designs to advanced graphic design jobs. We offer a variety of computer parts and repair services in-store, in-home, and remotely for our customers convenience. Aquilis is your fast, friendly, reliable, and expert computer repair company for all of your technical needs!</p>
 
         </details>
 
-        
-            <h1>Our company and founder</h1>
-            <p>Aquilis was founded by Nicholas Aquilante III in 2007, the first shop located in Glassport, Pennsylvania. What started as a home based business, simply completing the computer repairs in customers homes, expanding to operating two different shops, meeting the business needs of individuals and corporations both, while always keeping the option to work from a customer's home.</p>
-            <br/>
-            <p>Nicholas always had an affinity for working on and fixing computers, taking his first computer apart at the age of 12 and fixing his first one, his mother's at the age of 13, and knew he found his niche.</p>
+        <details className="company">
+            <summary className="comp-found">Our company and founder</summary>
+            <p>Aquilis was founded and started by Nicholas Aquilante III in 2007, in Glassport, Pennsylvania, around 20 minutes from the city center. Originally started as a home based business repairing computers in the customers homes, we have since expanded our work to working with corporations as well as still maintaining relationships and completing regular house calls for our clients, as well as adding in services for building and networking of computers.</p>
+            <br />
+            <p>Nicholas Aquilante III opened his first official shop in 2009 in Glassport one year after graduating South Allegheny High School. What started as a hobby and something he was interested in at 12 years old became his passion fixing his first computer for his first 'client', his mother at the age of 13 when her employer's tech staff could not figure out the issue and how to fix it.</p>
+            <br />
+            <p>In his free time when he is not repairing computers, Nicholas likes taking trips back to Happy Valley (having attended school at Penn State) and tailgating the football games or visiting friends.</p>
+            </details>        
 
             <h1>License Information</h1>
 
