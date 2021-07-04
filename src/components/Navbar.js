5aceb4d745f3dc456cab4d5d79c0019f4722a2ac
chris6661 <74381468+chris6661@users.noreply.github.com>
2021-07-04 18:45:04 -0500

    added correct jqery script tag for hamburger menu

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,62 +1,62 @@
 import React from 'react'
-// import {Link} from 'react-router-dom';
 //REACTFONT awesome
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome"; 
 import { faBars } from "@fortawesome/free-solid-svg-icons"; 
 import {Link} from 'react-router-dom';
 
 
+
 const Navbar = () => {
     return (
       <div className="nav-main">
-        <nav className="navbar navbar-expand-lg bg-dark">
-        <div className='container'>
-        <a className="navbar" href="/#"> </a>
-        <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-            <FontAwesomeIcon icon={faBars} style={{ color: '#fff'}}/>        
-        </button>
-      
-        <div className="collapse navbar-collapse" id="navbarSupportedContent">
-          <ul className="navbar-nav m-auto">
-
-            <li className="nav-item active">
-              <Link  to = "/" className="nav-link" href="/#">Home <span className="sr-only">(current)</span></Link>
-            </li>
-
-            <li className="nav-item">
-              <Link to = "/about"  className="nav-link" >About Us</Link>
-            </li>
-
-
-            <li className="nav-item">
-                <Link to = "/testimonials" className="nav-link">Testimonials</Link>
-            </li>
-            
-            <li className="nav-item">
-                <Link to = "/services" className="nav-link">Services</Link>
-            </li>
-                
-            <li className="nav-item">
-                <Link to = "/sell" className="nav-link">Sell</Link>
-            </li>
-                
-            <li className="nav-item">
-                <Link to = "/business" className="nav-link">Business Solutions</Link>
-            </li>
-            
-            <li className="nav-item">
-                <Link to = "/locations" className="nav-link">Locations</Link>
-            </li>
-            
-            <li className="nav-item">
-                <Link to = "/book" className="nav-link">Book Online</Link>
-            </li>
-        
-          </ul>
+          <nav className="navbar navbar-expand-lg bg-dark">
+            <div className='container'>
+              <a className="navbar" href="/#"> </a>
+                <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+                    <FontAwesomeIcon icon={faBars} style={{ color: '#fff'}}/>        
+                </button>
           
-        </div>
-        </div>
-      </nav>
+              <div className="collapse navbar-collapse" id="navbarSupportedContent">
+                <ul className="navbar-nav m-auto">
+
+                  <li className="nav-item active">
+                    <Link  to = "/" className="nav-link" href="/#">Home <span className="sr-only">(current)</span></Link>
+                  </li>
+
+                  <li className="nav-item">
+                    <Link to = "/about"  className="nav-link" >About Us</Link>
+                  </li>
+
+
+                  <li className="nav-item">
+                      <Link to = "/testimonials" className="nav-link">Testimonials</Link>
+                  </li>
+                  
+                  <li className="nav-item">
+                      <Link to = "/services" className="nav-link">Services</Link>
+                  </li>
+                      
+                  <li className="nav-item">
+                      <Link to = "/sell" className="nav-link">Sell</Link>
+                  </li>
+                      
+                  <li className="nav-item">
+                      <Link to = "/business" className="nav-link">Business Solutions</Link>
+                  </li>
+                  
+                  <li className="nav-item">
+                      <Link to = "/locations" className="nav-link">Locations</Link>
+                  </li>
+                  
+                  <li className="nav-item">
+                      <Link to = "/book" className="nav-link">Book Online</Link>
+                  </li>
+            
+                </ul>
+              
+              </div>
+            </div>
+        </nav>
       </div>
       
     )
