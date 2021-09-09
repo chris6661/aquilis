e3d34c4851932e77eae565fda33b1e92651877da
chris6661 <74381468+chris6661@users.noreply.github.com>
2021-09-09 00:54:23 -0500

    added first part of text to services componenet

--- src/pages/Services.js
+++ src/pages/Services.js
@@ -6,11 +6,29 @@ const Services = () => {
             {/* 3x3 grid is original site */}
             {/* topic, then goes in to what each one covers underneath */}
             {/* for development, start with h4 and 5 tags */}
-            <h4></h4>
+            <h4>Hardware Replacement</h4>
+            <p>We can fix it all from broken screens to crashed hard drives, memory or motherboard replacement, and liquid spill damage. Stop in to see how we can help!</p>
+                <h5>Repairs</h5>
+                    <p>We can fix almost anything both hardware and software for PC and Mac. If your computer is running slow, your screen needs replaced, or just need cosmetic damage fixed, call us today for an estimate or stop in for a diagnosis. We can fix these and more, and get you back up to speed!</p>
+                <h5>House Calls</h5>
+                    <p>Is there an issue that you need us to come and check out? If it easier for us to come to you instead of bringing everything to us, we make house calls in the Greater-Pittsburhg Area and some parts of Beaver, Butler, Fayette, Washington, and Westmoreland counties. Give us a call and book an appointment!</p>
 
-            <h4></h4>
 
-            <h4></h4>
+            <h4>Slow Computer?</h4>
+                <p></p>
+                    <h5></h5>
+                        <p></p>
+                    <h5></h5>
+                        <p></p>
+
+
+            <h4>Business IT Services, Support, and Consulting</h4>
+                <p></p>
+                    <h5></h5>
+                        <p></p>
+                    <h5></h5>
+                        <p></p>
+
 
         </div>
     )
