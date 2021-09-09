69a8a892117d43cb112f0a7514b0f3f7dac5ba89
chris6661 <74381468+chris6661@users.noreply.github.com>
2021-09-09 00:32:01 -0500

    styled footer to stay at bottom, added grid to sell component

--- src/pages/Sell.js
+++ src/pages/Sell.js
@@ -6,7 +6,7 @@ import desktop from "../images/desktop-stock.jpg";
 
 const Sell = () => {
     return (
-        <div>
+        
             <div className="container-fluid">
                 <div className ="row">
                     <div className="sales">
@@ -17,50 +17,54 @@ const Sell = () => {
                                 
                                 We offer custom built PCs for a variety of uses such as work, school, gaming, or even servers. Most custom PCs start at $499 and one of our techs will sit with you and design the best for your home or business. Additionally, our custom built PCs come with a better warranty than going to a "big box" store, and all custom built computers come with a minimum 3 year warranty free of charge, two years more than a typical "big box" store. <br/><br/>
                                 
-                                In store, we sell computer components such as hard drvies, memory, power supplies, motherboards and processors. We also sellaccessories such as keyboards, mice, flash drives, and more. Finally, we are able to help you tkae care of issues with screens or screen replacements, battery or charger issues, and other parts you may need to get back up and running!</p>
+                                In store, we sell computer components such as hard drvies, memory, power supplies, motherboards and processors. We also sell accessories such as keyboards, mice, flash drives, and more. Finally, we are able to help you tkae care of issues with screens or screen replacements, battery or charger issues, and other parts you may need to get back up and running!</p>
                         </div>
 
-                                <div className="col-xs-4 col-sm-4 col-md-4 col-lg-4 col-xl-4 sales-tablets">
-                                <img src={tablet} alt="phones and tablets" />
+                                <div className="container">
+                                    <div className="row row-cols-3">
+                                        <div className="col sales-tablets">
+                                        <img src={tablet} alt="phones and tablets" />
 
-                                    <h5>Tablets and Phones</h5>
-                                </div>
+                                            <h5>Tablets and Phones</h5>
+                                        </div>
 
-                                <div className="col-xs-4 col-sm-4 col-md-4 col-lg-4 col-xl-4 sales-monitors">
-                                <img src={monitor} alt="computer monitor" />
+                                        <div className="col sales-monitors">
+                                        <img src={monitor} alt="computer monitor" />
 
-                                    <h5>Monitors</h5>
-                                </div>
+                                            <h5>Monitors</h5>
+                                        </div>
 
-                                <div className="col-xs-4 col-sm-4 col-md-4 col-lg-4 col-xl-4 sales-macbooks">
-                                <img src={macbook} alt="macbook" />
+                                        <div className="col sales-macbooks">
+                                        <img src={macbook} alt="macbook" />
 
-                                    <h5>Macbooks</h5>
-                                </div>
+                                            <h5>Macbooks</h5>
+                                        </div>
 
-                                <div className="col-xs-4 col-sm-4 col-md-4 col-lg-4 col-xl-4 sales-desktops">
-                                <img src={desktop} alt="desktop pc" />
+                                        <div className="col sales-desktops">
+                                        <img src={desktop} alt="desktop pc" />
 
-                                    <h5>Desktops PCs</h5>
-                                </div>
+                                            <h5>Desktops PCs</h5>
+                                        </div>
 
-                                <div className="col-xs-4 col-sm-4 col-md-4 col-lg-4 col-xl-4 sales-windows">
-                                <img src={tablet} alt="phones and tablets" />
+                                        <div className="col sales-windows">
+                                        <img src={tablet} alt="phones and tablets" />
 
-                                    <h5>Windows</h5>
-                                </div>
+                                            <h5>Windows</h5>
+                                        </div>
 
-                                <div className="col-xs-4 col-sm-4 col-md-4 col-lg-4 col-xl-4 sales-parts">
-                                <img src={tablet} alt="phones and tablets" />
-
-                                    <h5>Parts and Accessories</h5>
+                                        <div className="col sales-parts">
+                                        <img src={tablet} alt="phones and tablets" />
+                                    
+                                            <h5>Parts and Accessories</h5>
+                                        </div>
+                                    </div>
                                 </div>
                         {/* 515 w 335 h */}
 
                     </div>
                 </div>
             </div>
-        </div>
+        
     )
 }
 
