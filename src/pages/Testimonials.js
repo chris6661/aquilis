3abea51c392b93413f808b61efb2a950680e0cc7
chris6661 <74381468+chris6661@users.noreply.github.com>
2021-10-01 23:53:37 -0500

    added mvp text to testimonial section

--- src/pages/Testimonials.js
+++ src/pages/Testimonials.js
@@ -2,8 +2,33 @@ import React from 'react'
 
 const Testimonials = () => {
     return (
-        <div>
-            test text for what people be saying about this place
+        <div className="container">
+
+            <div className="testimonials">
+                
+                <h1>Testimonials</h1>
+                    <p>If you're still not sure, see what both our current and previous customers have to say!</p>
+            
+            </div>                
+
+            <div className="test-body">
+
+                <p>"Great service. Great people. Affordblae and fast! My husband and I have had a very slow running computer and we were having issues getting out e-mails and Facebook. We dealt with it for a while then a close friend of mine recommended Aquilis. They came out to the house and were very fast. We got our memory upgraded to speed up our computer and Nick was very friendly and easy to work with. Before he left, he made sure everything was working and that I understood how to do everything right."</p>
+                    <span>Sandy Criscuolo - Customer - March 2015</span>
+
+                <p>"HIGHLY RECOMMENDED. Been using Aquilis for 5 years now from their first location. They have a shop but will come out to your house as well. Very pleased with their service, price, and reliability. So, a year ago we bought a new computer with Windows 7 from them. Custom built and a great price, and Nick (the owner) delivered it, set it up for us, and answered any questions I had. My husband and I have used Aquilis multiple times throughout the years and other family members have been recommended to them. Great price, speed, and friendly service! Thanks again!"</p>
+                    <span>Fran Woods - Customer - March 2015</span>
+
+                <p>"Purchased a laptop and printer from ere. Very satisfied. Great product & Great price. They were also very helpful. </p>
+                    <span>Kristy Behanna - Customer - February 2015</span>
+
+                <p>"Very knowledgeable.....instantly knew the problem.....my computer now runs like a brand new machine! Thanks again!</p>
+                    <span>Matt Sauers - Customer - January 2015</span>
+
+                
+
+            </div>
+            
         </div>
     )
 }
