50c089f22ca3cf344c1b61970096c12f24eff875
chris6661 <74381468+chris6661@users.noreply.github.com>
2021-10-04 23:50:39 -0500

    added border around round 1 of cards and buttons

--- src/pages/Book.js
+++ src/pages/Book.js
@@ -8,24 +8,30 @@ const Book = () => {
                 <h1>Book an appointment</h1>
             </div>
 
-            <div className="booking-services">
+                <div className="booking-services">
 
-                <h3>In Store Appointment</h3>
-                    <h5>1 hour</h5>
-                        <h6>$20</h6>
-                            <button>Book Now</button>
+                    <div className="card1">
+                        <h3>In Store Appointment</h3>
+                            <h5>1 hour</h5>
+                                <h6>$20</h6>
+                                    <button>Book Now</button>
+                    </div>
 
-                <h3>In Home Service Call</h3>
-                    <h5>1 hour</h5>
-                        <h6>$74.99</h6>
-                            <button>Book Now</button>
+                    <div className="card2">
+                        <h3>In Home Service Call</h3>
+                            <h5>1 hour</h5>
+                                <h6>$74.99</h6>
+                                    <button>Book Now</button>
+                    </div>
 
-                <h3>Commerical Service Call</h3>
-                    <h5>1 hour</h5>
-                        <h6>$99.99</h6>
-                            <button>Book Now</button>
-
-            </div>
+                    <div className="card3">
+                        <h3>Commerical Service Call</h3>
+                            <h5>1 hour</h5>
+                                <h6>$99.99</h6>
+                                    <button>Book Now</button>
+                    </div>
+                    
+                </div>
 
         </div>
     )
