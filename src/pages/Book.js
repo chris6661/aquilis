adbc4aaf856593d8aaacb93cf2a79330740fc202
chris6661 <74381468+chris6661@users.noreply.github.com>
2021-10-04 23:42:55 -0500

    first outline of book now mvp component

--- src/pages/Book.js
+++ src/pages/Book.js
@@ -2,8 +2,31 @@ import React from 'react'
 
 const Book = () => {
     return (
-        <div>
-            test text for reading
+        <div className="container-fluid">
+            
+            <div className="book-main">
+                <h1>Book an appointment</h1>
+            </div>
+
+            <div className="booking-services">
+
+                <h3>In Store Appointment</h3>
+                    <h5>1 hour</h5>
+                        <h6>$20</h6>
+                            <button>Book Now</button>
+
+                <h3>In Home Service Call</h3>
+                    <h5>1 hour</h5>
+                        <h6>$74.99</h6>
+                            <button>Book Now</button>
+
+                <h3>Commerical Service Call</h3>
+                    <h5>1 hour</h5>
+                        <h6>$99.99</h6>
+                            <button>Book Now</button>
+
+            </div>
+
         </div>
     )
 }
