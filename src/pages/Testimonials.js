fb1079ecbd68ab3f1b0e78a12f9623f91cbcb289
chris6661 <74381468+chris6661@users.noreply.github.com>
2021-10-04 23:21:10 -0500

    added placeholder for testimonials page for mvp, can not add embedd yer

--- src/pages/Testimonials.js
+++ src/pages/Testimonials.js
@@ -11,20 +11,33 @@ const Testimonials = () => {
             
             </div>                
 
+                <div className="review-box">
+                    <p>This is where the reviews will go once embedded, rotate a few out, add link to see more, hard code 3-7 in to site</p>
+                    <span>need to get nick to log in to be able to embed from google reviews and add actual reviews </span>
+                </div>
+
+                <br />
+
             <div className="test-body">
 
+                    <span></span>
                 <p>"Great service. Great people. Affordblae and fast! My husband and I have had a very slow running computer and we were having issues getting out e-mails and Facebook. We dealt with it for a while then a close friend of mine recommended Aquilis. They came out to the house and were very fast. We got our memory upgraded to speed up our computer and Nick was very friendly and easy to work with. Before he left, he made sure everything was working and that I understood how to do everything right."</p>
                     <span>Sandy Criscuolo - Customer - March 2015</span>
+
+                <br /><br/>
 
                 <p>"HIGHLY RECOMMENDED. Been using Aquilis for 5 years now from their first location. They have a shop but will come out to your house as well. Very pleased with their service, price, and reliability. So, a year ago we bought a new computer with Windows 7 from them. Custom built and a great price, and Nick (the owner) delivered it, set it up for us, and answered any questions I had. My husband and I have used Aquilis multiple times throughout the years and other family members have been recommended to them. Great price, speed, and friendly service! Thanks again!"</p>
                     <span>Fran Woods - Customer - March 2015</span>
 
+                    <br /><br/>
+
                 <p>"Purchased a laptop and printer from ere. Very satisfied. Great product & Great price. They were also very helpful. </p>
                     <span>Kristy Behanna - Customer - February 2015</span>
 
+                    <br /><br/>
+
                 <p>"Very knowledgeable.....instantly knew the problem.....my computer now runs like a brand new machine! Thanks again!</p>
                     <span>Matt Sauers - Customer - January 2015</span>
-
 
 
             </div>
