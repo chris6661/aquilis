57db8a48ba8096c8bc6175ed234234215ab94b0b
chris6661 <74381468+chris6661@users.noreply.github.com>
2021-10-15 01:31:24 -0500

    changed footer color to match page

--- src/pages/Locations.js
+++ src/pages/Locations.js
@@ -4,7 +4,7 @@ import {Link} from 'react-router-dom';
 const Locations = () => {
     return (
 
-        <div className="container">
+        <div className="container-fluid">
 
                 <div className="location">
                     <h1>Locations</h1>
