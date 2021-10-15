b832ec2355e59418124e837168c1d36db7b3fb97
chris6661 <74381468+chris6661@users.noreply.github.com>
2021-10-15 00:54:51 -0500

    put booking page in cards

--- src/pages/About.js
+++ src/pages/About.js
@@ -2,7 +2,9 @@ import React from 'react'
 
 const About = () => {
     return (
+
         <div className="container">
+
                 <div className="about">
                 <h1>Your computer repair experts</h1>
 
--- src/pages/Book.js
+++ src/pages/Book.js
@@ -6,31 +6,49 @@ const Book = () => {
             
             <div className="book-main">
                 <h1>Book an appointment</h1>
+                    <p>Click any of the links below to schedule an appointment with one of our technicians!</p>
             </div>
 
                 <div className="booking-services">
 
-                    <div className="card1">
-                        <h3>In Store Appointment</h3>
-                            <h5>1 hour</h5>
-                                <h6>$20</h6>
-                                    <button>Book Now</button>
+
+                    <div className="container">
+
+                        <div className="row">
+
+                            <div className="col">
+                                <div className="card1">
+                                    <h3>In Store Appointment</h3>
+                                        <h5>1 hour</h5>
+                                            <h6>$20</h6>
+                                                <button>Book Now</button>
+                                </div>
+                            </div>
+
+
+                            <div className="col">
+                                <div className="card2">
+                                    <h3>In Home Service Call</h3>
+                                        <h5>1 hour</h5>
+                                            <h6>$74.99</h6>
+                                                <button>Book Now</button>
+                                </div>
+                            </div>
+
+
+                            <div className="col">
+                                <div className="card3">
+                                    <h3>Commerical Service Call</h3>
+                                        <h5>1 hour</h5>
+                                            <h6>$99.99</h6>
+                                                <button>Book Now</button>
+                                </div>
+                            </div>
+                            
+                        </div>
+
                     </div>
 
-                    <div className="card2">
-                        <h3>In Home Service Call</h3>
-                            <h5>1 hour</h5>
-                                <h6>$74.99</h6>
-                                    <button>Book Now</button>
-                    </div>
-
-                    <div className="card3">
-                        <h3>Commerical Service Call</h3>
-                            <h5>1 hour</h5>
-                                <h6>$99.99</h6>
-                                    <button>Book Now</button>
-                    </div>
-                    
                 </div>
 
         </div>
